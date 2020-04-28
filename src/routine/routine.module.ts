f91bab6216ee558bf91f654f14cdda2152be01d3
DavidCarneros <davidcarneros4598@gmail.com>
2020-04-28 20:25:20 +0200

    Añadido modulo de rutina

---
+++ src/routine/routine.module.ts
@@ -0,0 +1,17 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Routine } from "./routine.entity";
+import { RoutineService } from "./routine.service";
+import { RoutineController } from "./routine.controller";
+
+
+@Module({
+    imports : [
+       TypeOrmModule.forFeature([Routine])
+    ],
+    providers: [RoutineService],
+    exports: [RoutineService],
+    controllers: [RoutineController]
+})
+
+export class RoutineModule {}
