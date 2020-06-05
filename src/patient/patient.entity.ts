f22e34235d2f2a21907414a4be55b135937521bb
DavidCarneros <davidcarneros4598@gmail.com>
2020-06-06 00:16:12 +0200

    Añadidas nuevas relaciones a la tabla patient

--- src/patient/patient.entity.ts
+++ src/patient/patient.entity.ts
@@ -1,6 +1,8 @@
 import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
 import { IsEmail, IsString } from "class-validator";
 import { Routine } from "src/routine/routine.entity";
+import { RoutinePatient } from "src/routine-patient/routine-patient.entity";
+import { RoutineResult } from "src/routine-result/routine-result.entity";
 
 @Entity()
 export class Patient {
@@ -15,7 +17,10 @@ export class Patient {
     @Column()
     key: string;
 
-    @OneToMany(type => Routine, routine => routine.patient)
-    routines: Routine[];
+    @OneToMany(type => RoutinePatient, routinePatient => routinePatient.patient)
+    routines: RoutinePatient[];
+
+    @OneToMany(type => RoutineResult, routineResult => routineResult.patient)
+    routineResults: RoutineResult[];
 
 }
--- src/patient/patient.service.ts
+++ src/patient/patient.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Patient } from './patient.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class PatientService {}
+export class PatientService {
+
+    constructor(
+        @InjectRepository(Patient)
+        private readonly patientRepository: Repository<Patient>
+    ) {}
+
+    async register(patient:Patient){
+        return await this.patientRepository.save(patient);
+    }
+
+    async getPatientByKey(key: string){
+        return await this.patientRepository
+            .createQueryBuilder("patient")
+            .select(["patient"])
+            .where(" key = :key",{key:key})
+            .getOne();
+    }
+
+
+}
